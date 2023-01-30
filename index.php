7985615f747178447b38bbdc48cec8418a9b1b6d
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-30 16:26:20 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/** op-unit-git:/Git.class.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-git
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+/** Include
+ *
+ */
+include(__DIR__.'/Git.class.php');
