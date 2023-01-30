7b4384c54529e7efa7b0e6d529bcda0269d938d6
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-31 02:29:51 +0900

    New: testcase > SubmoduleConfig

---
+++ testcase/SubmoduleConfig.php
@@ -0,0 +1,22 @@
+<?php
+/** op-unit-git:/testcase/SubmoduleConfig.class.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-git
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+D( OP()->Unit('Git')->SubmoduleConfig() );
