a41e2c84ee8c24d75755034a8268f1c9b8f2368c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-30 16:43:50 +0900

    New: ci > Git

---
+++ ci/Git.php
@@ -0,0 +1,30 @@
+<?php
+/** op-unit-git:/ci/Git.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-git
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP;
+
+//	...
+$ci = new CI();
+
+//	Test
+$result =  null;
+$args   = ['a','b'];
+$ci->Set('Test', $result, $args);
+
+//	...
+return $ci->GenerateConfig();
