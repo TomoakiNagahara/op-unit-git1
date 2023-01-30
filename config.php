a13d4629df5322024d458481b27b12e9cc47aa56
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-30 16:38:45 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/** op-unit-git:/Git.class.php
+ *
+ * Default config file.
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-git
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+//	...
+return [
+];
