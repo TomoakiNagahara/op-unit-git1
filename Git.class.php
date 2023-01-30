291e6561f345e070b532ad06ccf234412a5d69ea
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-30 16:21:02 +0900

    New: Git

---
+++ Git.class.php
@@ -0,0 +1,39 @@
+<?php
+/** op-unit-git:/Git.class.php
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-git
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** use
+ *
+ */
+use OP\IF_UNIT;
+use OP\OP_CORE;
+use OP\OP_CI;
+
+/** Git
+ *
+ * @created    2023-01-30
+ * @version    1.0
+ * @package    op-unit-git
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+class Git implements IF_UNIT
+{
+	use OP_CORE, OP_CI;
+}
